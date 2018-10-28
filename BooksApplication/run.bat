9097204d194a9ccd9527a6e16d4f36a0cd9bc838
Deepesh <deepeshch2k9@gmail.com>
2018-10-28 00:31:10 -0500

    improved readme, added docker-compose

---
+++ BooksApplication/run.bat
@@ -0,0 +1,5 @@
+SET PGCONNECTIONSTRING=Host=localhost;Username=postgres;Password=postgres;Database=booksappdb;Port=5432
+SET REDISCONNECTIONSTRING=localhost
+SET ELASTICCONNECTIONSTRING=http://localhost:9200
+SET MONGOCONNECTIONSTRING=mongodb://localhost:27017
+dotnet run
